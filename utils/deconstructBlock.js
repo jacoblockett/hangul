36655d43b908a26fdcbd553c3a951068e7624288
jacoblockett <jacobwyattlockett@gmail.com>
2024-05-19 22:17:57 -0400

    base -> baseIndex for clarity

--- utils/deconstructBlock.js
+++ utils/deconstructBlock.js
@@ -17,10 +17,10 @@ function deconstructBlock(syllable) {
 
 	if (syllable.length > 1 || codepoint < 0xac00 || codepoint > 0xd7a3) throw invalidError
 
-	const base = codepoint - 0xac00
-	const initialIndex = ~~(base / 588)
-	const medialIndex = ~~((base % 588) / 28)
-	const finalIndex = ~~(base % 28)
+	const baseIndex = codepoint - 0xac00
+	const initialIndex = ~~(baseIndex / 588)
+	const medialIndex = ~~((baseIndex % 588) / 28)
+	const finalIndex = ~~(baseIndex % 28)
 	const initial = String.fromCodePoint(initialIndex + 0x1100)
 	const medial = String.fromCodePoint(medialIndex + 0x1161)
 	const final = finalIndex ? String.fromCodePoint(finalIndex + 0x11a8 - 1) : 0
