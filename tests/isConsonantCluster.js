b68cd93ad516ff5d0e104b2a45fda3447bc82cb4
jacoblockett <jacobwyattlockett@gmail.com>
2024-10-25 23:41:54 -0400

    major test bug: yeah i copy pasta real good-like

--- tests/isConsonantCluster.js
+++ tests/isConsonantCluster.js
@@ -1,31 +1,31 @@
 import test from "node:test"
 import assert from "node:assert"
-import isConsonant from "../lib/isConsonant.js"
+import isConsonantCluster from "../lib/isConsonantCluster.js"
 
-test(`isConsonant() | expects: false | undefined input string`, () => {
-	assert.deepStrictEqual(isConsonant(), false)
+test(`isConsonantCluster() | undefined input string`, () => {
+	assert.deepStrictEqual(isConsonantCluster(), false)
 })
 
-test(`isConsonant("") | expects: false | empty input string`, () => {
-	assert.deepStrictEqual(isConsonant(""), false)
+test(`isConsonantCluster("") | empty input string`, () => {
+	assert.deepStrictEqual(isConsonantCluster(""), false)
 })
 
-test(`isConsonant("ㄲ") | expects: true | consonant cluster double letter`, () => {
-	assert.deepStrictEqual(isConsonant("ㄱ"), true)
+test(`isConsonantCluster("ㄲ") | consonant cluster double letter`, () => {
+	assert.deepStrictEqual(isConsonantCluster("ㄲ"), true)
 })
 
-test(`isConsonant("ㄳ") | expects: true | consonant cluster letter`, () => {
-	assert.deepStrictEqual(isConsonant("ㄱ"), true)
+test(`isConsonantCluster("ㄳ") | consonant cluster letter`, () => {
+	assert.deepStrictEqual(isConsonantCluster("ㄳ"), true)
 })
 
-test(`isConsonant("ㄱ") | expects: false | non-consonant cluster letter`, () => {
-	assert.deepStrictEqual(isConsonant("ㅏ"), false)
+test(`isConsonantCluster("ㄱ") | non-consonant cluster letter`, () => {
+	assert.deepStrictEqual(isConsonantCluster("ㄱ"), false)
 })
 
-test(`isConsonant("ㅏ") | expects: false | vowel letter`, () => {
-	assert.deepStrictEqual(isConsonant("ㅏ"), false)
+test(`isConsonantCluster("ㅏ") | vowel letter`, () => {
+	assert.deepStrictEqual(isConsonantCluster("ㅏ"), false)
 })
 
-test(`isConsonant("a") | expects: false | non-hangul letter`, () => {
-	assert.deepStrictEqual(isConsonant("a"), false)
+test(`isConsonantCluster("a") | non-hangul letter`, () => {
+	assert.deepStrictEqual(isConsonantCluster("a"), false)
 })
