e9f6a0406d9a3f4fc55a568945cd3bd24e7d09e8
jacoblockett <jacobwyattlockett@gmail.com>
2024-10-25 23:46:25 -0400

    init commit

---
+++ tests/isDiphthong.js
@@ -0,0 +1,23 @@
+import test from "node:test"
+import assert from "node:assert"
+import isDiphthong from "../lib/isDiphthong.js"
+
+test(`isDiphthong() | undefined input string`, () => {
+	assert.deepStrictEqual(isDiphthong(), false)
+})
+
+test(`isDiphthong("") | empty input string`, () => {
+	assert.deepStrictEqual(isDiphthong(""), false)
+})
+
+test(`isDiphthong("ㅘ") | diphthong letter`, () => {
+	assert.deepStrictEqual(isDiphthong("ㅘ"), true)
+})
+
+test(`isDiphthong("ㅏ") | non-diphthong letter`, () => {
+	assert.deepStrictEqual(isDiphthong("ㅏ"), false)
+})
+
+test(`isDiphthong("a") | non-hangul letter`, () => {
+	assert.deepStrictEqual(isDiphthong("a"), false)
+})
